9183b06a0cc2894fc8e9edca1152795980de8d87
Chad Smykay <csmykay@ion.local>
2020-04-23 10:27:49 -0500

    update build script

--- build-images.sh
+++ build-images.sh
@@ -2,9 +2,9 @@
 #https://docs.docker.com/engine/reference/commandline/build/
 DATE=$(date +%Y-%m-%d_%H:%M:%S)
 VERSION=2.0
-if [ -z $CURRENT ]; then
-	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-fi
+#if [ -z $CURRENT ]; then
+#	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+#fi
 docker build --build-arg BUILD_DATE=$DATE --build-arg BUILD_VERSION=$VERSION --no-cache=true --rm -f Dockerfile -t ess-hpecp-mofed-centos7.6-image:1.0 .
 docker tag ess-hpecp-mofed-centos7.6-image:$VERSION ess-hpecp-mofed-centos7.6-image:latest
 docker tag ess-hpecp-mofed-centos7.6-image csmykay/ess-hpecp-mofed-centos7.6-image:latest
