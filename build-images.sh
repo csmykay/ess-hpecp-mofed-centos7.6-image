3d7c7cc92a2e47e65a04bce40e65f09e1e0a12ca
Chad Smykay <csmykay@ion.local>
2020-05-08 10:38:31 -0500

    updated centos copy and build script to run onsite

--- build-images.sh
+++ build-images.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #https://docs.docker.com/engine/reference/commandline/build/
 DATE=$(date +%Y-%m-%d_%H:%M:%S)
-VERSION=3.0
+VERSION=1.0
 #if [ -z $CURRENT ]; then
 #	CURRENT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 #fi
-docker build --build-arg BUILD_DATE=$DATE --build-arg BUILD_VERSION=$VERSION --no-cache=true --rm -f Dockerfile -t ess-hpecp-mofed-centos7.6-image:1.0 .
+docker build --build-arg BUILD_DATE=$DATE --build-arg BUILD_VERSION=$VERSION --no-cache=true --rm -f Dockerfile -t ess-hpecp-mofed-centos7.6-image:$VERSION .
 docker tag ess-hpecp-mofed-centos7.6-image:$VERSION ess-hpecp-mofed-centos7.6-image:latest
-docker tag ess-hpecp-mofed-centos7.6-image:latest csmykay/ess-hpecp-mofed-centos7.6-image:latest
-docker login
-docker push csmykay/ess-hpecp-mofed-centos7.6-image:latest
+#docker tag ess-hpecp-mofed-centos7.6-image:latest csmykay/ess-hpecp-mofed-centos7.6-image:latest
+#docker login
+#docker push csmykay/ess-hpecp-mofed-centos7.6-image:latest
